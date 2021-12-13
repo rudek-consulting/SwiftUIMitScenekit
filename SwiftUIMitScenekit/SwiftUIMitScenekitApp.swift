53ed287fd438eb13bff595bbb3fb2967420915a5
Arne Rudek <mail@arne-rudek.de>
2021-12-13 11:08:17 +0100

    Initial Commit

---
+++ SwiftUIMitScenekit/SwiftUIMitScenekitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMitScenekitApp.swift
+//  SwiftUIMitScenekit
+//
+//  Created by Arne Rudek on 13.12.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMitScenekitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
